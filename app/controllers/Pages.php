628fddf357afe3129990d3eb15c4733933e1a041
Tiberius Mairura <tiberiusmairura90@gmail.com>
2021-11-20 16:24:30 +0300

    add pages

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -10,10 +10,34 @@ class Pages extends Controller{
             'title' => 'Home Page',
             'users' => $users
         ];
-        $this->view('pages/index', $data);
+        $this->view('pages/index', $data);      
+    }
+
+    public function login(){
+        $this->view('pages/login');
+    }
+
+    public function logout(){
+        $this->view('pages/logout');
+    }   
+
+    public function doctorList(){
+        $this->view('pages/doctorList');
+    }
+
+    public function doctorProfile(){
+        $this->view('pages/doctorProfile');
+    }
+
+    public function patients(){
+        $this->view('pages/patients');
     }
 
     public function about(){
+        $this->view('pages/about');
+    }
+
+    public function contact(){
         $this->view('pages/about');
     }
 }
