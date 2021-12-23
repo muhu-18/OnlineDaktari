0d46b6c072b8c442da51b883768a25c207d12b29
Tiberius Mairura <tiberiusmairura90@gmail.com>
2021-12-23 16:15:46 +0300

    Added core class files

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -5,11 +5,38 @@ class Pages extends Controller{
 //        $this->pageModel = $this->model('Page');
     }
     public function index(){
-
         $data = [
             'title' => 'Home Page'
         ];
-        $this->view('pages/index', $data);      
+        $this->view('pages/index', $data);
     }
 
+    public function about(){
+        $data = [
+            'title' => 'Home Page'
+        ];
+        $this->view('pages/about', $data);
+    }
+
+    public function doctors(){
+        $data = [
+            'title' => 'Home'
+        ];
+        $this->view('pages/doctors', $data);
+    }
+
+
+    public function services(){
+        $data = [
+            'title' => 'Services'
+        ];
+        $this->view('pages/doctors', $data);
+    }
+
+    public function contact(){
+        $data = [
+            'title' => 'Contact'
+        ];
+        $this->view('pages/contact', $data);
+    }
 }
